9d33a012efc1b0ac46d25639fbe8d08af0ec5bd7
Thanhntmany-ubuntu <thanhntmany@gmail.com>
2024-03-15 10:34:05 +0700

    sfile_get

--- src/c_cpp_modules/sfile/sfile.c
+++ src/c_cpp_modules/sfile/sfile.c
@@ -17,22 +17,20 @@ void freeIPage(SFile_IPage *ptr)
     free(ptr);
 };
 
-void sfile_init(SFileHandler *sf, FILE *io)
+void sfile_init(SFileHandler *sf, const char *filename)
 {
-    sf->io = io;
+    sf->io = fopen(filename, "a+");
     sf->root = mallocIPage();
 };
 
-void sfile_release(SFileHandler *sf){
-    // #TODO:
+void sfile_release(SFileHandler *sf)
+{
+    // #TODO: free pages
+    fclose(sf->io);
 };
 
 void sfile_get(SFileHandler *sf, SFile_Key *key, SFile_SegDec *segdec)
 {
-    printf("key->id --> %.8X : ", key->id);
-    printf(" %d - %d - %d - %d\n", key->p[3], key->p[2], key->p[1], key->p[0]);
-    printf("-----------------------------\n");
-
     void **o_ptr = &(sf->root->idx[key->p[3]]);
     if (!*o_ptr)
         *o_ptr = mallocIPage();
@@ -48,22 +46,42 @@ void sfile_get(SFileHandler *sf, SFile_Key *key, SFile_SegDec *segdec)
         if (!*o_ptr) // #TODO:
             abort();
         memset(*o_ptr, 0, sizeof(SFile_Page));
-        // #TODO: Load page segment in file
-        // Should use stream with buffer = sizeof(SFile_Page)
+
+        SFile_Key _key = *key;
+        _key.p[0] = 0;
+
+        fflush(sf->io);
+        fseek(sf->io, _key.id, SEEK_SET);
+        fread(*o_ptr, sizeof(SFile_Page), 1, sf->io);
     };
 
     *segdec = ((SFile_Page *)*o_ptr)->idx[key->p[0]];
 };
 
+void sfile_add(SFileHandler *sf, SFile_SegDec *segdec, SFile_Key *key)
+{
+    printf("offset: %.16lx\n", segdec->offset);
+    printf("size  : %.16lx\n", segdec->size);
+    // #TODO:
+};
+
+void sfile_set(SFileHandler *sf, SFile_SegDec *segdec, SFile_Key *key)
+{
+    printf("offset: %.16lx\n", segdec->offset);
+    printf("size  : %.16lx\n", segdec->size);
+    // #TODO:
+};
+
 int main()
 {
+
     SFileHandler sf;
-    sfile_init(&sf, NULL);
+    sfile_init(&sf, "./data..sfile_idx");
 
     printf("sizeof(sf)  : %ld\n", sizeof(sf));
     // printf("sf.root     : %p\n", sf.root);
     printf("sf.root[0]  : %p\n", sf.root->idx[0]);
-    printf("sf.root[255]: %ld\n", (long int)sf.root->idx[259]);
+    printf("sf.root[255]: %ld\n", (long int)sf.root->idx[255]);
     printf("============================\n");
 
     SFile_Key key = {.id = 0};
