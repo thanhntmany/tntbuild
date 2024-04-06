f89f839c32cdc2813447556a42952626a9041eef
Thanhntmany-ubuntu <thanhntmany@gmail.com>
2024-04-06 22:35:25 +0700

    memp: init

---
+++ src/c_cpp_modules/memp/memp.c
@@ -0,0 +1,137 @@
+#include <errno.h>    // errno
+#include <fcntl.h>    // open, F_WRLCK, , F_SETLK
+#include <stdlib.h>   // malloc, free
+#include <sys/stat.h> // S_IRUSR, S_IWUSR
+#include <unistd.h>   // close, SEEK_SET, sysconf, _SC_PAGESIZE, ssize_t
+#include <string.h>   // memcpy
+#include <sys/uio.h>  // iovec, preadv
+#include <malloc.h>   // memalign
+#include "memp.h"
+
+/******************************************************************************
+ *
+ * Main functions
+ *
+ */
+static struct memp_opt PMEM_DEFAULT_OPT = {.page_size = 0};
+struct memp *memp_open()
+{
+    struct memp *const restrict mp = malloc(sizeof(struct memp));
+
+    mp->anchor_page.offset = -1;
+    mp->anchor_page.next = mp->anchor_page.prev = &mp->anchor_page;
+    mp->anchor_page.buff = NULL;
+
+    memp_setotp(mp, &PMEM_DEFAULT_OPT);
+
+    return mp;
+};
+
+void memp_setotp(struct memp *const restrict mp, const struct memp_opt *const restrict otp)
+{
+    // page_size
+    size_t page_size = otp->page_size;
+    size_t sc_pagesize = (size_t)sysconf(_SC_PAGESIZE);
+    if (page_size < sc_pagesize)
+        page_size = sc_pagesize;
+
+    register size_t ps = 512;
+    while (ps < page_size)
+        ps <<= 1;
+
+    if (mp->page_size != ps)
+    {
+        memp_clear(mp);
+        mp->page_size = ps;
+    };
+};
+
+void memp_clear(struct memp *const restrict mp)
+{
+    struct memp_page *page = &mp->anchor_page;
+    while ((page = page->next)->buff)
+        free_page(page);
+};
+
+void memp_close(struct memp *const restrict mp)
+{
+    memp_clear(mp);
+    free(mp);
+};
+
+void *memp_locate(struct memp *const restrict mp, const off_t offset, struct memp_page **found_page)
+{
+    const off_t p_offset = offset % mp->page_size, page_offset = offset - p_offset;
+    struct memp_page *restrict page = mp->anchor_page.next;
+    if (page->offset != page_offset) // if not first page
+    {
+        register off_t o;
+        while ((o = (page = page->next)->offset) != page_offset)
+            if (o < 0)
+            {
+                page = load_page(mp, page_offset);
+                goto retpage;
+            };
+        (page->prev->next = page->next)->prev = page->prev;
+
+    retpage:
+        (((page->next = mp->anchor_page.next)->prev = page)->prev = &mp->anchor_page)->next = page; // move page to the first position
+    };
+    *found_page = page;
+    return p_offset;
+};
+
+off_t memp_locate_ptr(struct memp *const restrict mp, void *const restrict ptr, struct memp_page **found_page)
+{
+    const size_t page_size = mp->page_size;
+    struct memp_page *page = &mp->anchor_page;
+    size_t _p;
+    void *buff;
+
+    while ((buff = (page = page->next->buff)))
+        if (ptr >= buff && (_p = ptr - buff) < page_size)
+        {
+            // move page to the first position
+            (page->prev->next = page->next)->prev = page->prev;
+            (((page->next = mp->anchor_page.next)->prev = page)->prev = &mp->anchor_page)->next = page;
+            return (*found_page = page)->offset + _p;
+        };
+
+    return -1;
+};
+
+void memp_read(struct memp *const restrict mp, off_t offset, void *restrict buffer, size_t nbyte)
+{
+    const off_t page_size = mp->page_size;
+    struct memp_page *page;
+    void *ptr;
+    off_t load;
+    while (nbyte)
+    {
+        ptr = memp_locate(mp, offset, &page);
+        if ((load = page->buff + page_size - ptr) > nbyte)
+            load = nbyte;
+        memcpy(buffer, ptr, load);
+        offset += load;
+        buffer += load;
+        nbyte -= load;
+    };
+};
+
+void memp_write(struct memp *const restrict mp, off_t offset, void *restrict buffer, size_t nbyte)
+{
+    const off_t page_size = mp->page_size;
+    struct memp_page *page;
+    void *ptr;
+    off_t load;
+    while (nbyte)
+    {
+        ptr = memp_locate(mp, offset, &page);
+        if ((load = page->buff + page_size - ptr) > nbyte)
+            load = nbyte;
+        memcpy(ptr, buffer, load);
+        offset += load;
+        buffer += load;
+        nbyte -= load;
+    };
+};
---
+++ src/c_cpp_modules/memp/memp.h
@@ -0,0 +1,38 @@
+#ifndef IOFP_H
+#define IOFP_H
+
+#include <sys/file.h> // off_t
+#include <stdbool.h>  // bool
+
+struct memp_page
+{
+    off_t offset;
+    void *buff; /* Pointer to data.  */
+    struct memp_page *prev;
+    struct memp_page *next;
+};
+
+struct memp_opt
+{
+    size_t page_size; // bytes
+};
+
+struct memp
+{
+    struct memp_page anchor_page; // .memp_buffofpage is always NULL, .offset alway < 0 (-1)
+    size_t page_size;
+};
+
+struct memp *memp_open(); // init handler
+void memp_setotp(struct memp *const restrict mp, const struct memp_opt *const restrict otp);
+void memp_clear(struct memp *const restrict mp);
+void memp_close(struct memp *const restrict mp);
+
+void *memp_locate(struct memp *const restrict mp, const off_t offset, struct memp_page **found_page);
+off_t memp_locate_ptr(struct memp *const restrict mp, void *const restrict ptr, struct memp_page **found_page);
+#define memp_toendofpage(fp, offset) (fp->page_size - (offset % fp->page_size))
+
+void memp_read(struct memp *const restrict mp, off_t offset, void *restrict buffer, size_t nbyte);
+void memp_write(struct memp *const restrict mp, off_t offset, void *restrict buffer, size_t nbyte);
+
+#endif
